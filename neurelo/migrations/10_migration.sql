08d3f006fbd17158bcc0f4a06644095ebde127f9
Khang Phúc <khangpro592@gmail.com>
2024-06-26 10:49:32 +0000

    create first migration

---
+++ neurelo/migrations/10_migration.sql
@@ -0,0 +1,99 @@
+-- CreateTable
+CREATE TABLE
+  `Account` (
+    `Address` VARCHAR(191) NOT NULL,
+    `Email` VARCHAR(191) NOT NULL,
+    `Username` VARCHAR(191) NOT NULL,
+    PRIMARY KEY (`Email`)
+  ) DEFAULT CHARACTER
+SET
+  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE
+  `Billing` (
+    `AccountEmail` VARCHAR(191) NOT NULL,
+    `DateCreated` DATETIME (3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `DatePaid` DATETIME (3) NOT NULL,
+    `Id` INTEGER NOT NULL,
+    `Status` INTEGER NOT NULL,
+    `TotalPrice` DOUBLE NOT NULL,
+    PRIMARY KEY (`Id`)
+  ) DEFAULT CHARACTER
+SET
+  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE
+  `BillingDetail` (
+    `BillingId` INTEGER NOT NULL,
+    `Id` INTEGER NOT NULL,
+    `LegoId` INTEGER NOT NULL,
+    `Quantity` INTEGER NOT NULL,
+    PRIMARY KEY (`Id`)
+  ) DEFAULT CHARACTER
+SET
+  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE
+  `Image` (
+    `ImageId` INTEGER NOT NULL,
+    `ImageUrl` VARCHAR(191) NOT NULL,
+    `LegoId` INTEGER NOT NULL,
+    PRIMARY KEY (`ImageId`)
+  ) DEFAULT CHARACTER
+SET
+  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE
+  `Lego` (
+    `Description` VARCHAR(191) NOT NULL,
+    `Id` INTEGER NOT NULL,
+    `Image` VARCHAR(191) NOT NULL,
+    `Name` VARCHAR(191) NOT NULL,
+    `Number` VARCHAR(191) NOT NULL,
+    `Price` DOUBLE NOT NULL,
+    `ReleaseYear` INTEGER NOT NULL,
+    `ThemeId` INTEGER NOT NULL,
+    PRIMARY KEY (`Id`)
+  ) DEFAULT CHARACTER
+SET
+  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE
+  `LegoTheme` (
+    `Id` INTEGER NOT NULL,
+    `LegoId` INTEGER NOT NULL,
+    `ThemeId` INTEGER NOT NULL,
+    PRIMARY KEY (`Id`)
+  ) DEFAULT CHARACTER
+SET
+  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE
+  `Theme` (
+    `Id` INTEGER NOT NULL,
+    `Name` VARCHAR(191) NOT NULL,
+    PRIMARY KEY (`Id`)
+  ) DEFAULT CHARACTER
+SET
+  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Billing` ADD CONSTRAINT `Billing_AccountEmail_fkey` FOREIGN KEY (`AccountEmail`) REFERENCES `Account` (`Email`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BillingDetail` ADD CONSTRAINT `BillingDetail_LegoId_fkey` FOREIGN KEY (`LegoId`) REFERENCES `Lego` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Image` ADD CONSTRAINT `Image_LegoId_fkey` FOREIGN KEY (`LegoId`) REFERENCES `Lego` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LegoTheme` ADD CONSTRAINT `LegoTheme_LegoId_fkey` FOREIGN KEY (`LegoId`) REFERENCES `Lego` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LegoTheme` ADD CONSTRAINT `LegoTheme_ThemeId_fkey` FOREIGN KEY (`ThemeId`) REFERENCES `Theme` (`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
